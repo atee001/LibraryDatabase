a42c8ddd9fe8c56bc71f832ead900300369058ff
atee001 <atee001@ucr.edu>
2021-05-08 12:58:48 -0700

    Initial Commit

---
+++ header/Person.hpp
@@ -0,0 +1,16 @@
+#pragma once
+using namespace std;
+
+class Person{
+
+    string name, pass;
+
+  public:
+
+    Person(const string& name, const string& pass) : name(name), pass(pass){}
+    string getName() const{ return name;}
+    void setName(const string& name){ this->name = name;}
+    string getPass() const{ return pass;}
+    void setPass(const string& pass){ this->pass = pass;}
+
+};
