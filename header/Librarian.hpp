99d3e4367f9e4ec125d3be0c6d28b3cc35ea14d4
CJT578078 <CameronJustinTsai@gmail.com>
2021-05-08 16:00:44 -0700

    Added Librarian Class

---
+++ header/Librarian.hpp
@@ -0,0 +1,20 @@
+
+#pragma once
+
+#include "Person.hpp"
+
+#include <iostream>
+
+using namespace std;
+
+class Librarian : public Person {
+	
+	private:
+		//BookComponent* book;
+
+	public:
+	
+
+
+
+};
