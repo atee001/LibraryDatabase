32c4e3afb4183dce89db1b4aaf43321afa62628e
CJT578078 <CameronJustinTsai@gmail.com>
2021-05-08 17:47:25 -0700

    Added BookComponent.hpp

---
+++ header/BookComponent.hpp
@@ -0,0 +1,18 @@
+
+
+#pragma onnce
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class BookComponent {
+	private:
+
+	public:
+		virtual void display();
+		virtual void AddBook(BookComponent* book);
+		virtual RemoveBook(BookComponent* book);
+	
+};
