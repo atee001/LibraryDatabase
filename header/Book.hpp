05d310ae8f0a7e0bc42d96ee987e934bd3394cae
rohanbehera03 <65427904+rohanbehera03@users.noreply.github.com>
2021-05-15 01:08:09 -0700

    Update Book.hpp

--- header/Book.hpp
+++ header/Book.hpp
@@ -1,27 +1,29 @@
-#ifndef LIBRARYSYSTEM_BOOK_H
-#define LIBRARYSYSTEM_BOOK_H
+#ifndef LIBRARYSYSTEM_BOOK_HPP
+#define LIBRARYSYSTEM_BOOK_HPP
 
 
 #include <iostream>
 #include <string>
+#include "BookItem.hpp"
 using namespace std;
 
-
-class Book {
+class Book : public BookItem {
 private:
     string title;
     string author;
     string genre;
-    string type;
     int isbn;
+    
+protected:
+    string type;
 
-public:
-  
+public:  
+    Book();
     void display();
     void setTitle(const string& nameOfBook);
     void setAuthor(const string& nameOfAuthor);
     void setGenre(const string& nameOfGenre);
-    void setIsbn(const int& isbnNum);
+    void setIsbn(const long& isbnNum);
     string getTitle const();
     string getAuthor const();
     string getGenre const();
@@ -29,21 +31,25 @@ public:
 };
 
 class NonFiction : public Book {
-
+public:
+    NonFiction();
 };
 
-class Biographies : NonFiction {
-
+class Biographies : public NonFiction {
+public:
+    Biographies();
 };
 
-class Science : NonFiction {
-
+class Science : public NonFiction {
+public:
+    Science();
 };
 
-class travelGuides : NonFiction {
-
+class travelGuides : public NonFiction {
+public:
+    travelGuides();
 };
 
-#endif //LIBRARYSYSTEM_BOOK_H
+#endif //LIBRARYSYSTEM_BOOK_HPP
 
 
