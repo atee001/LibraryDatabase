43fbd4d0cba80f3f4534c816fd1444fa28cf6c49
rohanbehera03 <65427904+rohanbehera03@users.noreply.github.com>
2021-05-15 00:57:30 -0700

    Create Date.cpp

---
+++ src/Date.cpp
@@ -0,0 +1,35 @@
+#include "Date.hpp"
+
+Date::Date() {
+
+}
+
+Date::Date(int d, int m, int y) {
+    day = d;
+    month = m;
+    year = y;
+}
+
+void Date::printDate() {
+    cout << "Due date: " << month << "/" << day << "/" << year << endl;
+}
+
+int Date::getDay() {
+    return day;
+}
+void Date::setDay(int d) {
+    day = d;
+}
+int Date::getMonth() {
+    return month;
+}
+void Date::setMonth(int m) {
+    month = m;
+}
+
+int Date::getYear() {
+    return year;
+}
+void Date::setYear(int y) {
+    year = y;
+}
