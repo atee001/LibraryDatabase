e107f94d52c3d56aa7f9dbd6cfc59a7cdc67b0f6
rohanbehera03 <65427904+rohanbehera03@users.noreply.github.com>
2021-05-22 20:50:07 -0700

    Create LibraryCatalog.cpp

---
+++ header/LibraryCatalog.cpp
@@ -0,0 +1,12 @@
+#ifndef LIBRARYSYSTEM_LIBRARYCATALOG_H
+#define LIBRARYSYSTEM_LIBRARYCATALOG_H
+
+//class LibraryCatalog {
+//    Book search(SearchStrat*);
+//    void checkOut(User, key,  searchType);
+//    void print_search(ostream& out);
+
+//};
+
+
+#endif //LIBRARYSYSTEM_LIBRARYCATALOG_H
