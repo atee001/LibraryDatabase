669eba7b4055205139847a25a7cb4362e8a0080b
atee001 <atee001@ucr.edu>
2021-05-30 16:59:43 -0700

    Fixing Out of Bounds Map

--- src/SearchContains.cpp
+++ src/SearchContains.cpp
@@ -2,19 +2,19 @@
 
 vector<Book*> SearchContains::search(const BookRepository* b){ //verifies it's a right searchBy && searchQuery is correct
     
-    if(searchBy == "Author" && (b->getMapAuthor().find(searchQuery) != b->getMapAuthor().end())){ 
+    if(searchBy == "Author" && (b->getMapAuthor().count(searchQuery) != 0))  return b->getMapAuthor().at(searchQuery);
+ 
 	//for(auto it : b->getMapAuthor().find(searchQuery)->second){
 	
 	//	std::cout <<  "Found!!!! "  << it->getTitle() << " ";
 
 	//}
-	return b->getMapAuthor().at(searchQuery); 
 
-    }
+
     
-    else if (searchBy == "Title" && (b->getMapTitle().find(searchQuery) != b->getMapTitle().end())) return b->getMapTitle().at(searchQuery);
+    else if (searchBy == "Title" && (b->getMapTitle().count(searchQuery) != 0)) return b->getMapTitle().at(searchQuery);
     
-    else if (searchBy == "Genre"&& (b->getMapGenre().find(searchQuery) != b->getMapGenre().end())) return b->getMapGenre().at(searchQuery);
+    else if (searchBy == "Genre"&& (b->getMapGenre().count(searchQuery) != 0)) return b->getMapGenre().at(searchQuery);
     
     return vector<Book*>();
         
