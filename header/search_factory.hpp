460cca0c36b6d4527242e912e4f575755a42515e
atee001 <atee001@ucr.edu>
2021-05-30 21:30:22 -0700

    Search Factory

--- header/search_factory.hpp
+++ header/search_factory.hpp
@@ -1,21 +1,18 @@
 #pragma once
 #include "SearchStrat.hpp"
 #include "LibraryCatalog.hpp"
-
-
-
+#include <stack>
+#include "SearchOR.hpp"
+#include "SearchAND.hpp"
+#include "SearchContains.hpp"
+#include <iostream>
+using namespace std;
 class search_factory{
+	public:
 
-	SearchStrat* create(const string& searchBy, const string& searchQuery){
-	
-		if(searchBy == "")
-
-
-
-	}
-
-
-
-
+	SearchStrat* createField();
+	bool isValidField(const int& field);
+	bool isValidChoice(const int& choice);
+	SearchStrat* makeSearch();
 
 };
