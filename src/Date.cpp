b3670f198f92b6134d3a54938e0d9818b556ba7c
atee001 <atee001@ucr.edu>
2021-05-31 14:17:49 -0700

    Const reference for Date

--- src/Date.cpp
+++ src/Date.cpp
@@ -2,15 +2,18 @@
 
 Date::Date() {
 
+   day = 1;
+   month = 1;
+   year = 2021;
 }
 
-Date::Date(Date &d) {
+Date::Date(const Date &d) {
     month = d.getMonth();
     day = d.getDay();
     year = d.getYear();
 }
 
-Date::Date(int d, int m, int y) {
+Date::Date(const int& d, const int& m, const int& y) {
     day = d;
     month = m;
     year = y;
@@ -20,22 +23,22 @@ void Date::printDate() {
     cout << "Due date: " << month << "/" << day << "/" << year << endl;
 }
 
-int Date::getDay() {
+int Date::getDay() const{
     return day;
 }
-void Date::setDay(int d) {
+void Date::setDay(const int& d) {
     day = d;
 }
-int Date::getMonth() {
+int Date::getMonth() const {
     return month;
 }
-void Date::setMonth(int m) {
+void Date::setMonth(const int& m) {
     month = m;
 }
 
-int Date::getYear() {
+int Date::getYear() const{
     return year;
 }
-void Date::setYear(int y) {
+void Date::setYear(const int& y) {
     year = y;
 }
