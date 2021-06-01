7accdb127a5d0d3aca4a51077efe18301c92aecc
atee001 <atee001@ucr.edu>
2021-05-31 19:21:51 -0700

    Librarian class

--- header/Librarian.hpp
+++ header/Librarian.hpp
@@ -1,17 +1,25 @@
 #ifndef LIBRARYSYSTEM_LIBRARIAN_HPP
 #define LIBRARYSYSTEM_LIBRARIAN_HPP
-
 #include "Person.hpp"
 using namespace std;
 
 class Librarian: public Person {
-public:
+private:
+    string name, pass;
     double balance;
     double fine = 0.10;
     int days;
+public:
 
-    Librarian(string name, string pass);
+     bool getAdminStatus() {return true;} 
 
+    Librarian(string name, string pass) : Person() { 
+	this-> name= name; 
+	this->pass = pass;
+        admin = true;
+}
+
+    
     void chargeLateFee(double totalLateFee, int daysOverdue) {
         daysOverdue = days;
         cout << "Enter total days overdue: " << endl;
